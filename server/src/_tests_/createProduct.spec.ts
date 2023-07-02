630eafcc661d6f1fd427b725fc3495eacc6b190f
Angélica <angelica_shigematsu@outlook.com>
2023-07-02 11:11:30 -0300

    test create product

---
+++ server/src/_tests_/createProduct.spec.ts
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const User =  require('../schemas/User')
+const mongoURL= require('../config')
+
+const {expect,test, beforeAll, describe, afterAll, beforeEach, it} = require('@jest/globals');
+const Product =  require('../schemas/Product')
+
+describe('insert', () => {
+  beforeAll(async () => {
+    if(!process.env.MONGO_URL) {
+      throw new Error('MOngoDB server not initialized')
+    }
+
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  it('should able to insert a new product', async () => { 
+    const mockProduct = { 
+      description: 'Coelho',
+      stock: 3,
+      price: 38.0,
+      typeProduct: 'Arigurumi',
+      photo_link: 'https://images.unsplash.com/photo-1615486363973-f79d875780cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fHRveXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+    }
+
+    await Product.create(mockProduct)
+
+    const product = await Product.findOne({ description: mockProduct.description })
+
+    expect(mockProduct.description).toBe(product.description)
+  })
+})
